b4edad9c46673b640ab4dca077cd748c10740965
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-13 12:33:12 +0530

    variable added

--- train_model.py
+++ train_model.py
@@ -1,26 +1,49 @@
 import tensorflow as tf 
 import numpy as np 
+import matplotlib.pyplot as plt
 
 from model import simple_model
-from data_preprocessing import windowed_dataset
+from utils import windowed_dataset, plot_series
 from data_reader import load_data
 
 
 series, time = load_data()
 
-split_time = 2000
+split_time = 3000
 time_train = time[:split_time]
 X_train = series[:split_time]
 time_valid = time[split_time:]
 X_valid = series[split_time:]
 
+plot_series(time, series)
+plt.show()
+
 window_size = 30
 batch_size = 32
 shuffle_buffer_size  = 1000
 
-dataset = windowed_dataset(series, window_size, batch_size, shuffle_buffer_size)
-model = simple_model()
+print(X_train.shape)
+dataset = windowed_dataset(X_train, window_size, batch_size, shuffle_buffer_size)
+print(dataset)
+model = simple_model(window_size)
 
 lr_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10 ** (epoch/20))
 
-# model.fit()
+print("THIS REACHED ")
+history = model.fit(dataset, epochs = 100, verbose = 0, callbacks = [lr_schedule])
+print(history)
+
+
+forecast = []
+for time in range(len(series) - window_size):
+  forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
+
+forecast = forecast[split_time-window_size:]
+results = np.array(forecast)[:, 0, 0]
+
+
+plt.figure(figsize=(10, 6))
+
+plot_series(time_valid, X_valid)
+plot_series(time_valid, results)
+plt.show()
