13398d12edca8228f4b5ad32dad19b34e6e5f055
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-13 10:48:35 +0530

    typo fixed

--- data_reader.py
+++ data_reader.py
@@ -6,14 +6,14 @@ def load_data():
     time_step = []
     sunspots = []
 
-    with open('/data/Sunports.csv') as csvfile:
+    with open('data/Sunspots.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',')
         next(reader)
 
         for row in reader:
             sunspots.append(float(row[2]))
             time_step.append(float(row[0]))
-    
+    print(f'The lenght of the data is {len(time_step)}')
     return np.array(sunspots), np.array(time_step)
 
 
