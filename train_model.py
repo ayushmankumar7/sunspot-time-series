9146f19519e521926b53189ac340be694edc45d5
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-13 11:13:18 +0530

    stable point

--- train_model.py
+++ train_model.py
@@ -1,8 +1,8 @@
 import tensorflow as tf 
 import numpy as np 
 
-
-
+from model import simple_model
+from data_preprocessing import windowed_dataset
 from data_reader import load_data
 
 
@@ -17,3 +17,10 @@ X_valid = series[split_time:]
 window_size = 30
 batch_size = 32
 shuffle_buffer_size  = 1000
+
+dataset = windowed_dataset(series, window_size, batch_size, shuffle_buffer_size)
+model = simple_model()
+
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10 ** (epoch/20))
+
+# model.fit()
