2bcc320c7ffbd03390743bbe9b03bd68f3fe4652
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-13 12:32:55 +0530

    graphs added

--- model.py
+++ model.py
@@ -1,17 +1,19 @@
 import tensorflow as tf 
 import numpy as np 
 
-def simple_model():
+def simple_model(window_size):
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(10, activation ='relu'),
+        tf.keras.layers.Dense(100, activation ='relu', input_shape = [window_size]),
         tf.keras.layers.Dense(10, activation = 'relu'),
         tf.keras.layers.Dense(1)
     ])
     optimizer = tf.keras.optimizers.SGD(lr = 1e-8, momentum = 0.9)
     model.compile(
-        loss = tf.keras.losses.Huber(),
-        optimizer = optimizer
+        # loss = tf.keras.losses.Huber(),
+        optimizer = 'adam',
+        loss = 'mse'
     )
+    print("MODEL CALLED !")
     return model
 
