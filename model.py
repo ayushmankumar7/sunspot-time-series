cb2244f943fc9dab1c4c384b454b07a835e22dba
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-13 11:04:22 +0530

    simple_model added

--- model.py
+++ model.py
@@ -1,7 +1,17 @@
 import tensorflow as tf 
 import numpy as np 
 
-from data_reader import load_data
+def simple_model():
 
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(10, activation ='relu'),
+        tf.keras.layers.Dense(10, activation = 'relu'),
+        tf.keras.layers.Dense(1)
+    ])
+    optimizer = tf.keras.optimizers.SGD(lr = 1e-8, momentum = 0.9)
+    model.compile(
+        loss = tf.keras.losses.Huber(),
+        optimizer = optimizer
+    )
+    return model
 
-series, time = load_data()
