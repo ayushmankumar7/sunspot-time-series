0e397d4cf8f508779ce99888aa1fdffc8f9c6f27
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-15 13:27:45 +0530

    model saved and plotted

--- utils.py
+++ utils.py
@@ -12,6 +12,18 @@ def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
     return dataset
 
 
+
+def windowed_dataset1(series, window_size, batch_size, shuffle_buffer):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    return ds.batch(batch_size).prefetch(1)
+
+
+
 def plot_series(time, series, text = "",title ="", format="-", start=0, end=None):
     plt.plot(time[start:end], series[start:end], format)
     plt.xlabel("Time")
@@ -20,3 +32,13 @@ def plot_series(time, series, text = "",title ="", format="-", start=0, end=None
     plt.title(title)
     plt.grid(False)
 
+
+
+
+def model_forecast(model, series, window_size):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size))
+    ds = ds.batch(32).prefetch(1)
+    forecast = model.predict(ds)
+    return forecast
