57985231650f539131e2b540f311f33a10d82b5d
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-15 12:15:36 +0530

    conv_lstm added

--- model.py
+++ model.py
@@ -32,3 +32,21 @@ def lstm_model(window_size):
     )
 
     return model
+
+def conv_lstm(window_size):
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv1D(32, 5, 1, "casual", activation = 'relu', input_shape = [None, 1]),
+        tf.keras.layers.LSTM(32, return_sequences = True),
+        tf.keras.layers.LSTM(32),
+        tf.keras.layers.Dense(1),
+        tf.keras.layers.Lambda(lambda x : x*200)
+    ])
+
+    model.compile(
+        loss = tf.keras.losses.Huber(),
+        optimizer = tf.keras.optimizers.SGD(lr = 1e-6, momentum = 0.9)
+
+    )
+
+    return model
